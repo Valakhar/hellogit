dec169ff0027167bc3cd457597a6ec49e75eef23
Valakhar <Valakhar@users.noreply.github.com>
2017-09-13 15:54:56 -0400

    Added header file

--- client.cpp
+++ client.cpp
@@ -1,6 +1,8 @@
 // client
 #include "Game.h"
-#include Vector
+#include <vector>
+#include <iostream>
+using namespace std;
 int main()
 {
 vector <Game> v1;
@@ -8,7 +10,7 @@ v1.push_back (League);
 v1.push_back (Cod);
 v1.push_back (Fifa);
 v1.push_back (Battlefield);
-for (auto v:v2)
+for (auto v:v1)
 {
 cout <<v.getname()<<""<<v.getplaytime()<<endl;
 }
